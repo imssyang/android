883f582f172af35ed3fa75c3ca4b57a78a603208
imssyang <imssyang@gmail.com>
2022-07-15 00:38:43 +0800

    update env

--- setup/env.sh
+++ setup/env.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+# https://developer.android.com/studio/command-line/variables
 
 export ANDROID_DISABLE_ENV=yes
+
+export ANDROID_HOME=/opt/android/sdk
+export ANDROID_USER_HOME=/opt/android/.android
+
 export ANDROID_NDK=/opt/android/sdk/ndk/25.0.8775105
+
+export STUDIO_JDK=/opt/android/studio/jre
+#export STUDIO_PROPERTIES=
+#export STUDIO_VM_OPTIONS=
+#export STUDIO_GRADLE_JDK=
+
 eval "optbin -s /opt/android/bin"
 
