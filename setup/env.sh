2028a8a14759e66d803d833b2a477d8438d02d2d
imssyang <imssyang@gmail.com>
2022-07-17 20:43:15 +0800

    update setup

--- setup/env.sh
+++ setup/env.sh
@@ -4,11 +4,8 @@
 export ANDROID_DISABLE_ENV=yes
 
 export ANDROID_HOME=/opt/android/sdk
-#export ANDROID_USER_HOME=/opt/android/.android
-
+export ANDROID_USER_HOME=/opt/android/home
 export ANDROID_NDK=$ANDROID_HOME/ndk/25.0.8775105
-export ANDROID_EMULATOR_HOME=$ANDROID_HOME/emulator/user
-export ANDROID_AVD_HOME=$ANDROID_EMULATOR_HOME/avd
 
 export STUDIO_JDK=/opt/android/studio/jre
 #export STUDIO_PROPERTIES=
