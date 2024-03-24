f63dab7cc1f78bbcd5ccd7131186922183d35d1e
imssyang <imssyang@gmail.com>
2024-03-24 22:37:20 +0800

    feat: add macos branch

--- setup/env.sh
+++ setup/env.sh
@@ -2,15 +2,14 @@
 # https://developer.android.com/studio/command-line/variables
 
 export ANDROID_DISABLE_ENV=yes
-
 export ANDROID_HOME=/opt/android/sdk
 export ANDROID_USER_HOME=/opt/android/home
-export ANDROID_NDK=$ANDROID_HOME/ndk/25.0.8775105
+#export ANDROID_NDK=$ANDROID_HOME/ndk/25.0.8775105
 
-export STUDIO_JDK=/opt/android/studio/jre
+#export STUDIO_JDK=/opt/android/studio/jre
 #export STUDIO_PROPERTIES=
 #export STUDIO_VM_OPTIONS=
 #export STUDIO_GRADLE_JDK=
 
-eval "optbin -s /opt/android/bin"
+#eval "optbin -s /opt/android/bin"
 
